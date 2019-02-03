8209d41b9eb3d7776a7936ab64b84077b485e2a5
Admin <Admin@Admin-PC>
2019-02-03 13:37:18 +0530

    First level commit.

---
+++ src/main/java/org/zetahub/com/ZetaHub/ZetaHubApplication.java
@@ -0,0 +1,14 @@
+package org.zetahub.com.ZetaHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZetaHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZetaHubApplication.class, args);
+	}
+
+}
+
