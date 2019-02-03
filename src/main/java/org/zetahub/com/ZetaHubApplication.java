79d685f0cb49b9fc2be6fa2348ad8aaf46d1896f
Admin <Admin@Admin-PC>
2019-02-03 13:42:48 +0530

    commit

--- src/main/java/org/zetahub/com/ZetaHubApplication.java
+++ src/main/java/org/zetahub/com/ZetaHubApplication.java
@@ -1,4 +1,4 @@
-package org.zetahub.com.ZetaHub;
+package org.zetahub.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/org/zetahub/com/ZetaHubApplicationTests.java
+++ src/test/java/org/zetahub/com/ZetaHubApplicationTests.java
@@ -1,4 +1,4 @@
-package org.zetahub.com.ZetaHub;
+package org.zetahub.com;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
